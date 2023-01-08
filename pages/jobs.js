449091ad45fe172309189f1769fd6f450d50b392
hameedg <adilsaberi01@gmail.com>
2023-01-08 18:53:47 +0530

    about community added

---
+++ pages/jobs.js
@@ -0,0 +1,13 @@
+import Cardy from "../components/Cardy";
+import Navbar from "../components/Navbar";
+
+const jobs = () => {
+  return (
+    <div>
+      <Navbar />
+      <Cardy />
+    </div>
+  );
+};
+
+export default jobs;
