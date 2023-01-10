93490f0eb3fe220e26e3165b78a9ee790a3cb169
hameedg <adilsaberi01@gmail.com>
2023-01-10 22:13:30 +0530

    about community added

--- components/Home.jsx
+++ components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
             <div className=" md:gap-y-[2rem] py-[4.3rem] gap-x-[4rem] flex justify-center items-center md:pt-[6.7rem] md:flex-col lg:pt-[5.8rem] lg:pb-[3rem] ">
               <div className="max-w-[460px] leading-[59.04px]  mb-0 flex-shrink-0 flex-grow-0 basis-auto z-[1] flex flex-col items-start lg:static lg:pb-[2.2rem] md:w-auto md:max-w-[none] md:text-center md:items-center">
                 <h1
-                  className={`text-[3rem] font-clash sm:text-[2.1rem] sm: mt-[50px] tracking-normal sm:leading-[3rem] leading-[3.75rem] font-[600] mb-8 sm-mid-tablet:text-3xl sm-mid-tablet:leading-snug`}
+                  className={`text-[3rem] font-['ClashDisplay-Variable'] sm:text-[2.1rem] sm: mt-[50px] tracking-normal sm:leading-[3rem] leading-[3.75rem] font-[600] mb-8 sm-mid-tablet:text-3xl sm-mid-tablet:leading-snug`}
                 >
                   Discover your dream career with work experience
                 </h1>
