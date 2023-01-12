7f116eaada9fab94f74304ead4fe562befd83e96
hameedg <adilsaberi01@gmail.com>
2023-01-12 14:34:32 +0530

    changes in about

--- pages/earn.js
+++ pages/earn.js
@@ -1,14 +1,14 @@
 const earn = () => {
   return (
     <>
-      <div className="w-[100vw] h-[100vh]">
+      <div className="">
         <div className="flex flex-row flex-wrap">
-          <div className="max-w-[50vw] h-[100vh] relative z-[1] bg-[#f3f3f5]"></div>
-          <div>
+          <div className="w-[50vw] relative z-[1] bg-[#f3f3f5]"></div>
+          <div className="max-w-[40vw] ">
             <p className="text-[80px] font-[700] font-['ClashDisplay-Variable'] text-[#949498] ml-[6rem] mt-[8rem]">
               Learn.<br></br> Upskill.<br></br> Get rewarded{" "}
             </p>
-            <div className="max-w-[40vw] ml-[6rem] mt-[2rem] text-[1.125rem] leading-[2rem] font-inter">
+            <div className="ml-[6rem] mt-[2rem] text-[1.125rem] leading-[2rem] font-inter">
               It&#39;s time to get rewarded for learning new skills,
               implementing them in real time helping startups build the next big
               thing and gaining work experience
