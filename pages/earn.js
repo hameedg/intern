bd480afd0506544730c336238fd057c74c8ed153
hameedg <adilsaberi01@gmail.com>
2023-01-12 14:32:39 +0530

    changes in about

--- pages/earn.js
+++ pages/earn.js
@@ -3,7 +3,7 @@ const earn = () => {
     <>
       <div className="w-[100vw] h-[100vh]">
         <div className="flex flex-row flex-wrap">
-          <div className="w-[50vw] h-[100vh] relative z-[1] bg-[#f3f3f5]"></div>
+          <div className="max-w-[50vw] h-[100vh] relative z-[1] bg-[#f3f3f5]"></div>
           <div>
             <p className="text-[80px] font-[700] font-['ClashDisplay-Variable'] text-[#949498] ml-[6rem] mt-[8rem]">
               Learn.<br></br> Upskill.<br></br> Get rewarded{" "}
