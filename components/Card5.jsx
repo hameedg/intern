d67c1fcf4377f31c5637d27adff27b85802ed0b9
hameedg <adilsaberi01@gmail.com>
2023-01-13 15:48:24 +0530

    work progressed

--- components/Card5.jsx
+++ components/Card5.jsx
@@ -8,7 +8,7 @@ const Card5 = (props) => {
         ></img>
       </div>
       <div className="flex flex-col gap-y-[8px] sm:items-start">
-        <p className="sm:mt-[5px] pb-0 text-[1.125rem] font-[500] font-['ClashDisplay-Variable']">
+        <p className="sm:mt-[5px] pb-0 text-[1.125rem] font-[600] font-['ClashDisplay-Variable']">
           {props.title}
         </p>
         <p className="text-[#495057] text-[1rem] tracking-[-0.015em] leading-[1.75rem] ">
