238c344f3c53b7eafa610badfcf6b260cf827523
hameedg <adilsaberi01@gmail.com>
2023-01-16 23:38:01 +0530

    work progressed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -129,7 +129,7 @@ const Navbar = () => {
               <path d="M 3 7 A 1.0001 1.0001 0 1 0 3 9 L 27 9 A 1.0001 1.0001 0 1 0 27 7 L 3 7 z M 3 14 A 1.0001 1.0001 0 1 0 3 16 L 27 16 A 1.0001 1.0001 0 1 0 27 14 L 3 14 z M 3 21 A 1.0001 1.0001 0 1 0 3 23 L 27 23 A 1.0001 1.0001 0 1 0 27 21 L 3 21 z"></path>
             </svg>
           </div>
-          {!open ? (
+          {open ? (
             <div className="flex fixed left-0 top-0 right-0 bottom-0 mt-[4.125rem] flex-col items-start dm:hidden">
               <div className="opacity-[1] items-start w-[100%] h-[100%] p-[3.375rem] flex-col bg-[#1b1b1b] flex">
                 <div className="flex flex-col">
