075ab05c98658b0fbb80f3c508db9ae3639713cb
hameedg <adilsaberi01@gmail.com>
2023-01-26 16:26:08 +0530

    changes improved

--- components/Point.jsx
+++ components/Point.jsx
@@ -3,7 +3,7 @@ const Point = (props) => {
     <div>
       <div className="flex mb-[16px] justify-start items-center font-inter">
         <div className="gl:bg-[#2667FF] gl:rounded-[50%] gl:w-[28px] gl:h-[28px] mr-[16px] gl:text-center text-[1rem] font-inter text-[#fff] bg-[#2667FF] rounded-[50%] w-[8px] h-[8px]"></div>
-        <div className="p-[0.25rem] font-[400] ms:text-[1.125rem] text-[1rem] leading-[1.7] tracking-[-0.015em] md:text-[15px] flex items-center">
+        <div className="p-[0.25rem] font-[400] ms:text-[1.125rem] text-[1rem] leading-[1.7] tracking-[-0.015em] flex items-center">
           {props.name}
         </div>
       </div>
