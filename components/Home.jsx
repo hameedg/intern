fa62d7848705a6ec329574fb45addf8b8c584882
smartgrad07 <smartgrad07@gmail.com>
2023-03-08 19:31:49 +0530

    changes updated

--- components/Home.jsx
+++ components/Home.jsx
@@ -7,15 +7,15 @@ const Home = () => {
             <div className=" md:gap-y-[2rem] py-[4.3rem] gap-x-[4rem] flex justify-center items-center md:pt-[6.7rem] md:flex-col lg:pt-[5.8rem] lg:pb-[3rem] ">
               <div className="max-w-[700px] leading-[59.04px]  mb-0 flex-shrink-0 flex-grow-0 basis-auto z-[1] flex flex-col items-start lg:static lg:pb-[2.2rem] md:w-auto md:max-w-[none] md:text-center md:items-center">
                 <h1
-                  className={`text-[3rem] font-['ClashDisplay-Variable'] sm:text-[2.1rem] sm: mt-[50px] tracking-normal sm:leading-[3rem] leading-[3.75rem] font-[600] mb-8 sm-mid-tablet:text-3xl sm-mid-tablet:leading-snug`}
-                >
-                  Discover your dream career with work experience
+                  className={`text-[3rem] font-['ClashDisplay-Variable'] sm:text-[2.1rem] sm: mt-[50px] tracking-normal sm:leading-[3rem] leading-[3.75rem] font-[600] mb-8 sm-mid-tablet:text-3xl sm-mid-tablet:leading-snug`}>
+                  Changing the way students get internships
                 </h1>
                 <p className=" text-[1.125rem] font-[400] tracking-[-0.015em] leading-[1.7] font-inter mb-8 mt-0 text-[#9f9f9f]">
-                  Get the unique opportunity to gain real time work experience
-                  collaborating with peers across the world being a high-impact
-                  team member, helping startups build the next big thing, and
-                  receiving career mentorship from leading tech companies
+                  A proof-of-talent internship platform that helps students get
+                  internships through "virtual internship quests" aimed at
+                  making students stand out with a kickass proof of work
+                  demonstrating their skills helping startups build the next big
+                  thing
                 </p>
                 <div className="flex items-center flex-col">
                   <div className="blue-button py-[0.875rem] text-[1rem] leading-[1.7] tracking-[-0.015em] font-inter ">
--- components/Point.jsx
+++ components/Point.jsx
@@ -1,8 +1,26 @@
 const Point = (props) => {
   return (
     <div>
-      <div className="flex mb-[16px] justify-start items-center font-inter">
-        <div className="gl:bg-[#2667FF] gl:rounded-[50%] gl:w-[28px] gl:h-[28px] mr-[16px] gl:text-center text-[1rem] font-inter text-[#fff] bg-[#2667FF] rounded-[50%] w-[8px] h-[8px]"></div>
+      <div className="flex gap-x-[1rem] gap-y-[0.5rem] justify-start items-center font-inter sm:items-start">
+        <div className="flex ">
+          <svg
+            width="28"
+            height="28"
+            viewBox="0 0 28 28"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <g clip-path="url(#clip0_421_8584)">
+              <path
+                d="M13.9997 2.33301C7.55967 2.33301 2.33301 7.55967 2.33301 13.9997C2.33301 20.4397 7.55967 25.6663 13.9997 25.6663C20.4397 25.6663 25.6663 20.4397 25.6663 13.9997C25.6663 7.55967 20.4397 2.33301 13.9997 2.33301ZM13.9997 19.833C13.358 19.833 12.833 19.308 12.833 18.6663V13.9997C12.833 13.358 13.358 12.833 13.9997 12.833C14.6413 12.833 15.1663 13.358 15.1663 13.9997V18.6663C15.1663 19.308 14.6413 19.833 13.9997 19.833ZM15.1663 10.4997H12.833V8.16634H15.1663V10.4997Z"
+                fill="#2667FF"></path>
+            </g>
+            <defs>
+              <clipPath id="clip0_421_8584">
+                <rect width="28" height="28" fill="white"></rect>
+              </clipPath>
+            </defs>
+          </svg>
+        </div>
         <div className="p-[0.25rem] font-[400] ms:text-[1.125rem] text-[1rem] leading-[1.7] tracking-[-0.015em] flex items-center">
           {props.name}
         </div>
